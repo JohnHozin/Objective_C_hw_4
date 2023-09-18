1724e3f38f6549eff9813e228fe3e5b4550b0a4e
john <sim@mail.ru>
2023-09-18 06:56:03 -0700

    Initial Commit

---
+++ Objective_C_hw_4/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective_C_hw_4
+//
+//  Created by умпет on 18.09.2023.
+//  Copyright © 2023 Evgen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
